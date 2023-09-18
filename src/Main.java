d3a4e39c2582aabcb988e689c8e5c020e4c1786f
n3mochaken <ns.mixa@gmail.com>
2023-09-18 09:58:33 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,32 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //task1();
-        //task2();
-        //task3();
+        task1();
+        task2();
+        task3();
         }
+    public static void task1 (){
+        int a=12,b=27,c=44,d=15,e=9;
+        int result=a*(b+(c-(d*e)));
+        System.out.println(result);
+        result = result * -1;
+        System.out.println(result);
+        result = result * -1;
+        System.out.println(result);
+    }
+    public static void task2() {
+        int a=5, b=7;
+        a = a*b;
+        b = a/b;
+        a = a/b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static void task3(){
+        int a = 764, b;
+        a = (a/10)%10;
+        b = a;
+        System.out.println(b);
+    }
 
     }
