7f3f81fa8d4496cf869ec8600f1b61b0e2292f1d
n3mochaken <ns.mixa@gmail.com>
2023-09-18 10:03:18 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,27 @@ public class Main {
         task2();
         task3();
         }
-
-
+    public static void task1 (){
+        int a=12,b=27,c=44,d=15,e=9;
+        int result=a*(b+(c-(d*e)));
+        System.out.println(result);
+        result = result * -1;
+        System.out.println(result);
+        result = result * -1;
+        System.out.println(result);
+    }
+    public static void task2() {
+        int a=5, b=7;
+        a = a*b;
+        b = a/b;
+        a = a/b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static void task3(){
+        int a = 764, b;
+        a = (a/10)%10;
+        b = a;
+        System.out.println(b);
+    }
     }
